8395947a6374eecf2d70ed3627b0bee57685d1e5
mgoo <andrewmcghienz@gmail.com>
2016-04-22 22:46:19 +1200

    added MotorController class

---
+++ Main Project/src/MotorController.h
@@ -0,0 +1,21 @@
+/*
+ * MotorController.h
+ *
+ *  Created on: 21/04/2016
+ *      Author: mcghieandr
+ *
+ */
+
+#ifndef MOTORCONTROLLER_H_
+#define MOTORCONTROLLER_H_
+
+#include "Controller.h"
+
+class MotorController: public Controller{
+private:
+	MotorController();
+public:
+	virtual ~MotorController();
+};
+
+#endif /* MOTORCONTROLLER_H_ */
