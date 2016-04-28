e0734492d1ca1bc92cdab19368e632226e786aa1
mgoo <andrewmcghienz@gmail.com>
2016-04-28 12:08:31 +1200

    Fixed the name

---
+++ Main Project/src/CameraController.h
@@ -0,0 +1,20 @@
+/*
+ * CameraController.h
+ *
+ *  Created on: 21/04/2016
+ *      Author: mcghieandr
+ *
+ */
+
+#ifndef CAMERACONTROLLER_H_
+#define CAMERACONTROLLER_H_
+
+#include "Controller.h"
+
+class CameraController: public Controller{
+private:
+	CameraController();
+public:
+	virtual ~CameraController();
+};
+#endif /* CAMERACONTROLLER_H_ */
