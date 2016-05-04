dacd7f2ae510643a9f6af53117f23035607fd493
Andrew <andrewmcghienz@gmail.com>
2016-05-04 16:42:32 +1200

    removed other Controllers for simplicity

--- Main Project/src/main.cpp
+++ Main Project/src/main.cpp
@@ -11,9 +11,9 @@ extern "C" int  Sleep(int sec, int  usec);
 
 int main(){
 	InitHardware();
-	NetworkController *network_controller = (NetworkController *)NetworkController::getInstance();
-	MotorController *motor_controller = (MotorController *)MotorController::getInstance();
-	IRController *IR_controller = (IRController *)IRController::getInstance();
+	//NetworkController *network_controller = (NetworkController *)NetworkController::getInstance();
+	//MotorController *motor_controller = (MotorController *)MotorController::getInstance();
+	//IRController *IR_controller = (IRController *)IRController::getInstance();
 	CameraController *camera_controller = (CameraController *)CameraController::getInstance();
 	printf("it worked");
 	return  0;
