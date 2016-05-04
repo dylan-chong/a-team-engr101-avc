7b8dfad7dad5886a558a48d7bf619aec4470fe3c
Dylan Chong <dylan.chong@icloud.com>
2016-05-04 14:43:16 +1200

    Modified the template main.cpp for Jonah to work on

--- Temporary Code/IR/main.cpp
+++ Temporary Code/IR/main.cpp
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 
-extern "C" int init(int d_lev);
-extern "C" int take_picture();
-extern "C" char get_pixel(int row,int col,int colour);
-extern "C" int Sleep(int sec, int usec);
+extern "C" int init_hardware();
+extern "C" int read_analog(int ch_adc);
+extern "C" int sleep(int sec, int  usec);
 
 int main(){
-    //This sets up the RPi hardware and ensures
-    //everything is working correctly
-    init(0);
-    
+
+
+    // TODO get some readings from the IR sensors to make sure its working
+
     printf("Done");
     return 0;
+}
+
+int getDistance() {
+    int distance = 0;
+
+    // TODO
+
+    return distance;
 }
