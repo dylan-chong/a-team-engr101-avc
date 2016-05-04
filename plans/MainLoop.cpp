7f5dd367e7ab8e05c21858ba06ba7014a1095e7c
Dylan Chong <dylan.chong@icloud.com>
2016-05-05 11:08:00 +1200

    Added some more main loop design pseudo code.

--- plans/MainLoop.cpp
+++ plans/MainLoop.cpp
@@ -1,55 +1,109 @@
 #include <stdio.h>
 
-
 // NOTE: This is only pseudo code for 
 // jotting down the jist of things. Syntax is very 
 // very un-C.
 
 
-char * runNetwork() {
+// NOTE: the funny syntax means put this method inside the NetworkControler clas
+char * NetworkController.run(MovementController *movement) {
 	// do network stuff and 
 	// return error msg or NULL
 	return NULL;
 }
 
-char * runLine() {
+char * LineController.run(MovementController *movement) {
+
+	/* 
+
+	maybe just return an enum
+		typedef enum { // line is...
+			SLIGHTLY_TO_LEFT,
+			SLIGHTLY_TO_RIGHT,
+			FAR_TO_LEFT,
+			FAR_TO_RIGHT,
+			FAR_TO_LEFT_AND_RIGHT,
+			NO_LINE
+		} CameraResult;
+	and have a camera.update method
+
+	need camera processed value?
+	or just ask camera class for 
+		- isLineSlightlyToLeft() or right
+		- doesLineHaveSharpLeft() and right
+
+	*/
 	return NULL;
 }
 
-char * runMaze() {
+char * MazeController.run(MovementController *movement) {
+
+	while (true) {
+		float leftDistance = IR.getLeftDistance(); // use real class name instead of IR
+		float middleDistance = IR.getMiddleDistance();
+		float rightDistance = IR.getRightDistance();
+
+		MazeAction action = 
+			controller.getAction(leftDistance, middleDistance, rightDistance);
+
+		switch (action) { // tell movement to do stuff
+			case GO_STRAIGHT:
+				break;
+			case TURN_LEFT:
+				break;
+			case TURN_RIGHT:
+				break;
+		}
+	}
+	/*
+	
+
+	typedef enum {
+		GO_STRAIGHT,
+		TURN_LEFT,
+		TURN_RIGHT,
+		//REGRESS?
+	} MazeAction;
+
+
+
+	*/
+
 	return NULL;
 }
 
 char * runFinished() {
 	// spin around in circles, 
 	// after passing the finish line
+
+	// or don't get too cocky and just stay still
 	return "Too much win";
 }
 
-
 char * run() {
 	{
+		
 		char error[] = runNetwork();
 		if (error) {
-			return "Error during Network State: " + error;
+			return "Network State: " + error;
 		}
 	}
 	{
 		char error[] = runLine();
 		if (error) {
-			return "Error during Line State: " + error;
+			return "Line State: " + error;
 		}
 	}
 	{
 		char error[] = runMaze();
 		if (error) {
-			return "Error during Maze State: " + error;
+			return "Maze State: " + error;
 		}
 	}
 	{
 		char error[] = runFinished();
 		if (error) {
-			return "Error during Finished State: " + error;
+			return "Finished State: " + error;
 		}
 	}
 
@@ -60,7 +114,7 @@ int main() {
 
 	char error[] = run();
 	if (error != NULL) {
-		printf("\n\n%s\n\n", error);
+		printf("\n\nError during %s\n\n", error);
 	}
 
 	printf("%s\n", "Done");
