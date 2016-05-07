a19a2f58a20fee2036ab7d635b6a95433228a448
mgoo <andrewmcghienz@gmail.com>
2016-05-07 18:21:18 +1200

    removed obselete code and comments

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,17 +6,12 @@
 #include "MotorController.h"
 #include "IRController.h"
 
-// these two init methods were in the merge conflict
-// Andrew remove this comment once you see it
 extern "C" int  init_hardware();
-extern "C" int  init(int d_lev); 
 extern "C" int  sleep(int sec, int  usec);
 
 int main(){
 	init_hardware();
-	//init(0);
 	NetworkController *network_controller = NetworkController::makeInstance("192.168.1.2",22);
-
 	MotorController *motor_controller = MotorController::makeInstance();
 	IRController *IR_controller = IRController::makeInstance();
 	CameraController *camera_controller = CameraController::makeInstance();
