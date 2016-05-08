78b837ec4673fcd7fd8611f7df24a02eeec82167
Dylan Chong <dylan.chong@icloud.com>
2016-05-09 09:43:30 +1200

    Tidied up Ben's code some more (auto-indented using CLion)

--- test-modules/camera/main.cpp
+++ test-modules/camera/main.cpp
@@ -1,10 +1,15 @@
+// ****************** TEMPLATE STUFF ******************
+
+/*
 #include <stdio.h>
 #include <time.h>
 
+
 extern "C" int init(int d_lev);
 extern "C" int take_picture();
-extern "C" char get_pixel(int row,int col,int colour);
+extern "C" char get_pixel(int row, int col, int colour);
 extern "C" int Sleep(int sec, int usec);
+*/
 
 long getLineValue() {
     long value = 0;
@@ -36,64 +41,64 @@ int main() {
 #include <time.h>
 
 
-
 extern "C" int init(int d_lev);
 extern "C" int take_picture();
-extern "C" char get_pixel(int row,int col,int colour);
+extern "C" char get_pixel(int row, int col, int colour);
 extern "C" int Sleep(int sec, int usec);
-extern "C" int set_motor(int motor,int speed);
+extern "C" int set_motor(int motor, int speed);
 
-int main(){
+int main() {
     init(0);
     char c;
     // possibly not the best place to put this loop (while true cause why not),
     //im going to change how its laid out just try test with this
-    while(true){
-		int whiteness[320]; //the first array getting the camera input
-		int white_thr = 140; //whether the number is white enough
-		int white_compare[320]; // the second array to compare to (- on left, + on the right)
-		int white_final[320]; // what should be the multiplied version of the two above arrays
-		 
-		int sum = 0; //the sum of all the white final numbers added
-		int n_whites = 0; // a count of how many white spots have been counted( to check if we can still see the line)
-		
-		for(int i=0; i < 320; i++) {
-		   take_picture();
-		   c = get_pixel(160,120,3); //get pixel "whiteness" from centre of image 
-		   
-		   //Prints read pixel value 
-		   if(c > white_thr) {
-			   // if its greater than the threshold, the number is a one, and therefore white, so count increases
-			   whiteness[i] = 1;
-			   n_whites++;
-		   } else {
-			   whiteness[i] = 0;
-		   }
-		   white_compare[i] = i-160;
-		   white_final[i] = whiteness[i] * white_compare[i];
-		} 
+    while (true) {
+        int whiteness[320]; //the first array getting the camera input
+        int white_thr = 140; //whether the number is white enough
+        int white_compare[320]; // the second array to compare to (- on left, + on the right)
+        int white_final[320]; // what should be the multiplied version of the two above arrays
 
-		   // what should be the sum of white final working
-	   for (int i=0; i<320;i++){           
-			sum = sum + white_final[i];
-	   }
-	   // trying to make it so if it loses the line it reverses to try make it find it again
-	   if (n_whites == 0){
-		   leftMotor = -35;
-		   rightMotor = -35;
-		   set_motor(1,rightMotor);
-		   set_motor(2,leftMotor);
-	   } else {
-		   leftMotor = 35 + -sum * 0.004; // the 0.004 is just a random small number i took, change it if you want 
-		   rightMotor = 35 + sum *0.004;
-		   set_motor(1,rightMotor);
-		   set_motor(2,leftMotor);
-		}
+        int sum = 0; //the sum of all the white final numbers added
+        int n_whites = 0; // a count of how many white spots have been counted( to check if we can still see the line)
+
+        for (int i = 0; i < 320; i++) {
+            take_picture();
+            c = get_pixel(160, 120, 3); //get pixel "whiteness" from centre of image
+
+            //Prints read pixel value
+            if (c > white_thr) {
+                // if its greater than the threshold, the number is a one, and therefore white, so count increases
+                whiteness[i] = 1;
+                n_whites++;
+            } else {
+                whiteness[i] = 0;
+            }
+            white_compare[i] = i - 160;
+            white_final[i] = whiteness[i] * white_compare[i];
+        }
+
+        // what should be the sum of white final working
+        for (int i = 0; i < 320; i++) {
+            sum = sum + white_final[i];
+        }
+        // trying to make it so if it loses the line it reverses to try make it find it again
+        if (n_whites == 0) {
+            leftMotor = -35;
+            rightMotor = -35;
+            set_motor(1, rightMotor);
+            set_motor(2, leftMotor);
+        } else {
+            leftMotor = 35 + -sum * 0.004; // the 0.004 is just a random small number i took, change it if you want
+            rightMotor = 35 + sum * 0.004;
+            set_motor(1, rightMotor);
+            set_motor(2, leftMotor);
+        }
     }
-	set_motor(1,0);
-	set_motor(2,0);
-	
-	return 0;
+
+    set_motor(1, 0);
+    set_motor(2, 0);
+
+    return 0;
 }
 
 
