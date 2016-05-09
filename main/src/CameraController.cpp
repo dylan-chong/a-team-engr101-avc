97ec973721119ba1eb033555f2c95bc7887d851f
Andrew <andrewmcghienz@gmail.com>
2016-05-09 13:35:55 +1200

    added code form test-modules

--- main/src/CameraController.cpp
+++ main/src/CameraController.cpp
@@ -9,10 +9,18 @@
 
 #include "CameraController.h"
 
+extern "C" int take_picture();
+extern "C" char get_pixel(int row, int col, int colour);
+extern "C" int Sleep(int sec, int usec);
+
 //Controller* CameraController::instance;
 
 //This is the constructor
-CameraController::CameraController() {
+CameraController::CameraController(){
+	WHITE_THRESHOLD = 140;
+	center = 120;
+	currentError = 0;
+	prevousError = 0;
 }
 
 //this is the destructor
@@ -23,6 +31,32 @@ CameraController::~CameraController() {
 CameraController* CameraController::makeInstance(){
 	instance = new CameraController();
 	return (CameraController*)instance;
+}
+
+//gets an array of 1's and 0's that indicate blank and white pixels
+int[] CameraController::getWhiteArray(){
+	take_picture();
+	int white[320];
+	for (int count = 0; count<320; count++){
+		white[count]=get_pixel(count,120,3) > WHITE_THRESHOLD;
+	}
+	return white;
+}
+
+//gets the total sum of the white array * the position on the white pixels from the center
+int CameraController::sum(int white[320]){
+	int total = 0;
+	for (int count = 0; count<320; count++){
+		total += white[count]*(count-center);
+	}
+	return total;
+}
+
+//finds the change in error and updates the previous error variable
+int CameraController::differential(int sum){
+	int temp = previousError;
+	previousError = sum;
+	return (sum-temp)/temp;
 }
 
 
--- main/src/CameraController.h
+++ main/src/CameraController.h
@@ -14,10 +14,17 @@
 class CameraController: public Controller{
 friend class CameraControllerTest;
 private:
+	const int WHITE_THRESHOLD;
+	int center;
+	int previousError;
 
 public:
 	static CameraController* makeInstance();
 	virtual ~CameraController();
 	CameraController();
+
+	int[] getWhiteArray();
+	int sum(int white[]);
+	int differential(int sum);
 };
 #endif /* CAMERACONTROLLER_H_ */
