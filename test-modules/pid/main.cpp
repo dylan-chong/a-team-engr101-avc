534093cb51d926a4771a6e33f899547f0cf06c9c
Dylan Chong <dylan.chong@icloud.com>
2016-05-11 14:44:07 +1200

    Edited sleep function in PID (and copied some code from the wiki, but it is commented out)

--- test-modules/pid/main.cpp
+++ test-modules/pid/main.cpp
@@ -2,12 +2,40 @@
 #include  <time.h>
 
 extern "C" int init_hardware();
-extern "C" int Sleep(int sec, int usec);
+extern "C" int sleep(int sec, int usec);
 extern "C" int set_motor(int motor, int speed);
 
 int main() {
     printf("test\n");
-    Sleep(1,0);
+    sleep(1, 0);
     printf("end\n");
     return 0;
 }
+
+// Copy pasted from wiki (slightly modified for readability)
+/*
+int proportional() {
+
+}
+
+int integral() {
+
+}
+
+int derivative() {
+    int current_error = 0;
+    int kd = 5.0;
+
+
+    for (int i = 0; i < 320; i++) {
+        error = (i - 160) * get_pixel(i, 120, 3);
+        current_error = current_error + error;
+    }
+
+    Sleep(0, 100000);
+    int derivative_signal = (current_error - previous_error / 0.1) * kd;
+    int previous_error = current_error;
+    printf("Derivative signal is: %d", derivative_signal);
+    set_motor(1, derivative_signal);
+}
+ */
