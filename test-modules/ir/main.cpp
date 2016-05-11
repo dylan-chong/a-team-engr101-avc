a7021ed8b9a7d784fcdc7c611e4ac83f80e27f80
Jonah Ease <easejona@vuw.ac.nz>
2016-05-11 14:18:22 +1200

    debugging

--- test-modules/ir/main.cpp
+++ test-modules/ir/main.cpp
@@ -22,7 +22,7 @@ float getDistanceFromSensor(int sensorPin) {
 	printf("%d",read_analog(sensorPin));
 	printf("         ");
 
-	float distance = 26/1024*read_analog(sensorPin) + 4 ; //4 is the offset as readings are in the range of 4-30 cm
+	float distance = (26/1024)*read_analog(sensorPin) + 4 ; //4 is the offset as readings are in the range of 4-30 cm
 
 	return distance;
 }
