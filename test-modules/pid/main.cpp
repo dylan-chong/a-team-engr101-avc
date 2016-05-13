9a77ab9204769bac0ba100e5cf617d2f146dffce
Dylan Chong <dylan.chong@icloud.com>
2016-05-13 14:50:05 +1200

    Added testing TODOs, and fixed some mistakes from my refactoring

--- test-modules/pid/main.cpp
+++ test-modules/pid/main.cpp
@@ -9,32 +9,34 @@ extern "C" int set_motor(int motor, int speed);
 const float KP = 0.5;
 const float KD = 5.0;
 
-int IMG_WIDTH = 320;
+int IMG_WIDTH = 320; // TODO LATER reference the camera module for constants?
 
 // Don't need this.
 //int getPixelGreyscale(int x, int y) {
 //    return get_pixel(x, y, 3);
 //}
 
-// Use the value from the camera module, eventually
+// TODO Use the value from the camera module
+// For testing, inject fake values
 int getLineValue() {
-
+    return 1234; // TODO change
 }
 
-// Refactored code from wiki
+// Very refactored code from wiki
 
 int getProportional(int row, int lineValue) {
     int proportional_signal = lineValue * kp;
-    set_motor(1, (proportional_signal / (160 * 1 * kp)) * 255);
+    int motorVal = (proportional_signal / ((IMG_WIDTH / 2) * 1 * KP)) * 255;
+    set_motor(1, motorVal);
 
     printf("Proportional signal is: %d\n", proportional_signal);
 }
 
 // refreshPeriod is in seconds
-int getDerivative(int row, int lineValue, int previous_error, float refreshPeriod) {
+int getDerivative(int row, int lineValue, int previousLineValue, float refreshPeriod) {
 
-//    Sleep(0, (int) refreshPeriod * 1e6); // needs to be a sleep function in here to allow for time
-    int derivative_signal = (lineValue - previous_error / refreshPeriod) * kd; // add brackets around (current_error - previous_error) ?
+    int derivative_signal = (lineValue - previousLineValue / refreshPeriod) * KD; // add brackets around (current_error - previous_error) ?
+
     printf("Derivative signal is: %d\n", derivative_signal);
 
     set_motor(1, derivative_signal);
@@ -43,15 +45,27 @@ int getDerivative(int row, int lineValue, int previous_error, float refreshPerio
 // Integral apparently not necessary for AVC according to wiki
 int getIntegral() { return 0;}
 
+// TODO use the put everything together formula
+
 //
 
 int main() {
-    // currently just for testing the sleep functions
+
+    printf("\nTEST SLEEP\n\n");
+
+    // TODO currently just for testing the sleep functions
     printf("test\n");
     sleep(1, 0);
     printf("test 2\n");
     Sleep(1, 0);
     printf("test 3\n");
+
+    printf("\nTEST BEDMAS\n\n");
+
+    // TODO test order of operations
+    float a = (4 - 2 / 3);
+    float b = ((4 - 2) / 3);
+    printf("\ta = %f,\n\tb = %f\n", a, b);
 
     return 0;
 }
