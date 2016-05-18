e0fe3491a98cf80a94739a14e9b5c5ea0d88ca71
Jonah Ease <easejona@vuw.ac.nz>
2016-05-18 17:21:50 +1200

    Made a new test module combining IR and Motor

--- test-modules/ir-motor-test/main.cpp
+++ test-modules/ir-motor-test/main.cpp
@@ -4,6 +4,7 @@
 extern "C" int init(int d_lev);
 extern "C" int read_analog(int ch_adc);
 extern "C" int sleep(int sec, int usec);
+extern "C" int set_motor(int motor, int speed);
 
 ///Setting whole bunch of default values
 
@@ -15,6 +16,61 @@ bool thresholdSet = false;
 double lastLeftDistance = 0;
 double lastRightDistance = 0;
 double lastMiddleDIstance = 0;
+
+
+// The controller using a MotorController will access
+// this variable. The MotorController itself doesn't use it
+const float PERPENDICULAR_TURN_TIME = 1.2345; // DANIEL calibrate this
+
+const int FORWARD_SPEED = 30;
+const int BACKWARD_SPEED = -20;
+const int ROTATE_SPEED = 30; // should always be positive
+
+// NOTE: dont put any sleeps in any of these methods
+
+
+// just set the motors to full forward
+void moveForward() {
+    // OLD motor speeds - most updated ones in MotorController.h
+    set_motor(2, 200);//FORWARD_SPEED); //right motor
+    set_motor(1, 230);//FORWARD_SPEED); //left motor
+}
+
+void moveBackward() {
+    set_motor(2, BACKWARD_SPEED); //made reversing slower for higher accuracy in path correction
+    set_motor(1, BACKWARD_SPEED);
+}
+
+void stopMovement() {
+    set_motor(2, 0);
+    set_motor(1, 0);
+}
+
+// turn left on the spot
+void rotateLeft() {
+    set_motor(2, -ROTATE_SPEED);
+    set_motor(1, ROTATE_SPEED);
+}
+
+// turn right on the spot
+void rotateRight() {
+    set_motor(2, ROTATE_SPEED);
+    set_motor(1, -ROTATE_SPEED);
+}
+
+// when percent is 1.0 don't actually turn
+// when percent is -1.0 turn left on the spot
+// DANIEL maybe instead make 0 move straight
+// and 1.0 turn on the spot - whatever makes most sense
+// then edit this description
+void arcLeft(double percent) {
+    // set right to full speed;
+    // set left full speed * percent or whatever
+}
+
+void arcRight(double percent) {
+    // similar thing as arcLeft
+}
 
 float getDistanceFromSensor(int sensorPin) {
 
@@ -77,7 +133,9 @@ int main() {
 	init(0);
 
 	while (true) {
-		update();
+		double[] data update();
+		printf("%f",data[0]);
+		printf("%f",data[1]);
 	}
 
 
