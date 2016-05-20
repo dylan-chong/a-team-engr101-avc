acba2b596d062157a12227383314ce768b595dad
Dylan Chong <dylan.chong@icloud.com>
2016-05-20 19:12:25 +1200

    Used my new methods in the implementation of the existing public motor methods

--- test-modules/movement/main.cpp
+++ test-modules/movement/main.cpp
@@ -5,68 +5,20 @@ extern "C" int init(int x);
 extern "C" int sleep(int sec, int usec);
 extern "C" int set_motor(int motor, int speed);
 
+// ******************** PRIVATE IMPLEMENTATION ******************** //
+
 // The controller using a MotorController will access
 // this variable. The MotorController itself doesn't use it
 const float PERPENDICULAR_TURN_TIME = 1.2345; // DANIEL calibrate this
 
-const int FORWARD_SPEED = 30;
-const int BACKWARD_SPEED = -20;
-const int ROTATE_SPEED = 30; // should always be positive
-
 // NOTE: dont put any sleeps in any of these methods
 
 
-// just set the motors to full forward
-void moveForward() {
-    // OLD motor speeds - most updated ones in MotorController.h
-    set_motor(2, 200);//FORWARD_SPEED); // left motor
-    set_motor(1, 230);//FORWARD_SPEED); // right motor
-}
-
-void moveBackward() {
-    set_motor(2, BACKWARD_SPEED); //made reversing slower for higher accuracy in path correction
-    set_motor(1, BACKWARD_SPEED);
-}
-
-void stopMovement() {
-    set_motor(2, 0);
-    set_motor(1, 0);
-}
-
-// turn left on the spot
-void rotateLeft() {
-    set_motor(2, -ROTATE_SPEED);
-    set_motor(1, ROTATE_SPEED);
-}
-
-// turn right on the spot
-void rotateRight() {
-    set_motor(2, ROTATE_SPEED);
-    set_motor(1, -ROTATE_SPEED);
-}
-
-// when percent is 1.0 don't actually turn
-// when percent is -1.0 turn left on the spot
-// DANIEL maybe instead make 0 move straight
-// and 1.0 turn on the spot - whatever makes most sense
-// then edit this description
-void arcLeft(double percent) {
-    // set right to full speed;
-    // set left full speed * percent or whatever
-}
-
-void arcRight(double percent) {
-    // similar thing as arcLeft
-}
-
-
-// **************** DYLAN'S STUFF ****************
-
 // Refer to even-speeds.txt for nicer formatting of data
-// Array starts with reverse speeds (since motor is backwards)
 const int RIGHT_SPEEDS[18] = {
-    65, 41, 30, 20, 10, 0, 
-    -10, -20, -30, -41, -60, -70, -88, -95, -180, -200, -255
+        65, 41, 30, 20, 10, // reverse
+        0, // definitely stop
+        -10, -20, -30, -41, -60, -70, -88, -95, -180, -200, -255 // forward
 };
 
 void freezeIfSpeedOutOfRange(int speed) {
@@ -79,13 +31,13 @@ void freezeIfSpeedOutOfRange(int speed) {
     }
 }
 
-// both setLeft and setRight take values between
+// Both setLeft and setRight take values between
 // -5 and 11 inclusive
 // setting both to -3 to 2 inclusive means the robot wont move
 void setLeft(int speed) {
     freezeIfSpeedOutOfRange(speed);
     set_motor(2, speed * 10);
-} 
+}
 
 void setRight(int speed) {
     freezeIfSpeedOutOfRange(speed);
@@ -97,16 +49,51 @@ void moveStraightAtSpeed(int speed) {
     setRight(speed);
 }
 
+// ******************** PUBLIC METHODS ******************** //
 
+// just set the motors to full forward
+void moveForward() {
+    setLeft(5);
+    setRight(5);
+}
+
+void moveBackward() {
+    setLeft(-5);
+    setRight(-5);
+}
+
+void stopMovement() {
+    setLeft(0);
+    setRight(0);
+}
+
+// turn left on the spot
+void rotateLeft() {
+    setLeft(-5);
+    setRight(5);
+}
+
+// turn right on the spot
+void rotateRight() {
+    setLeft(5);
+    setRight(-5);
+}
+
+// -1 is max left
+// 0 is max forward
+// 1 is max right
+void arc(double direction) {
+    setLeft(5 * direction);
+    setRight(-5 * direction);
+}
+
+// ******************** TESTING ONLY ******************** //
 
 int main() {
     init(0);
 
-    /* Test complete*/
-
-    moveStraightAtSpeed(11);
-    sleep(3, 0);
-    moveStraightAtSpeed(11);
+    // todo test rotateLeft and right movement
+    rotateLeft();
     sleep(3, 0);
 
     stopMovement();
@@ -114,8 +101,6 @@ int main() {
 
     return 0;
 }
-
-
 
 /*
 // Daniels old code for moving in a straight line
