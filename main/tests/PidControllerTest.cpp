89bb31608138299180921139c8a99c4b6f9ddc52
mgoo <andrewmcghienz@gmail.com>
2016-05-21 12:30:51 +1200

    added the pidtest class

---
+++ main/tests/PidControllerTest.cpp
@@ -0,0 +1,18 @@
+/*
+ * PidControllerTest.cpp
+ *
+ *  Created on: 21/05/2016
+ *      Author: root
+ */
+
+#include "PidControllerTest.h"
+
+PidControllerTest::PidControllerTest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PidControllerTest::~PidControllerTest() {
+	// TODO Auto-generated destructor stub
+}
+
