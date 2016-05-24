7525a49144a6ea9338cfb7b0e02310f81564a8a4
Dylan Chong <dylan.chong@icloud.com>
2016-05-25 10:30:43 +1200

    Attemped to fix stop motor function

--- test-modules/stop/main.cpp
+++ test-modules/stop/main.cpp
@@ -2,7 +2,7 @@
 #include  <time.h>
 
 extern "C" int init(int x);
-extern "C" int sleep(int sec, int usec);
+extern "C" int Sleep(int sec, int usec);
 extern "C" int set_motor(int motor, int speed);
 
 int main() {
@@ -11,7 +11,7 @@ int main() {
     set_motor(1,0);
     set_motor(2,0);
 
-    sleep(1,0);
+    Sleep(1,0);
 
     return 0;
 }
