a87f29e8ff1fb8c9940c028a74ac4a2fdd054558
Dylan Chong <dylan.chong@icloud.com>
2016-05-24 18:00:50 +1200

    Tided up camera code

--- main/src/CameraController.cpp
+++ main/src/CameraController.cpp
@@ -15,9 +15,9 @@ extern "C" char get_pixel(int row, int col, int colour);
 extern "C" int Sleep(int sec, int usec);
 
 //This is the constructor
-CameraController::CameraController(){
-	previousError = 0;
-	dir = 'l';
+CameraController::CameraController() {
+    previousError = 0;
+    dir = 'l';
 }
 
 //this is the destructor
@@ -25,52 +25,52 @@ CameraController::~CameraController() {
 
 }
 
-CameraController* CameraController::makeInstance(){
-	instance = new CameraController();
-	return (CameraController*)instance;
+CameraController *CameraController::makeInstance() {
+    instance = new CameraController();
+    return (CameraController *) instance;
 }
 
 //gets the total sum of the white array * the position on the white pixels from the center
-int CameraController::getSum(int row){
-	    //the first array getting the camera input
-	    int whiteness[320];
-	    //the sum of all the white final numbers added
-	    int sum = 0;
-	    // a count of how many white spots have been counted( to check if we can still see the line)
-	    int n_whites = 0;
-	    take_picture();
-	    for (int y = 0; y < row; y+=2){
-			for (int i = 0; i < 320; i++) {
-				whiteness[i] = (get_pixel(i, y, 3) > WHITE_THRESHOLD);
-				n_whites += whiteness[i];
-				sum += whiteness[i] * (i - 160);
-			}
-	    }
-	    sum/=(row/2);
-	    // trying to make it so if it loses the line it reverses to try make it find it again
-	    if (n_whites == 0) {
-	    	dir = 's';
-	        return -1;
-	    }else {
-	    	if(sum>0){
-	    		dir = 'l';
-	    	} else {
-	    		dir = 'r';
-	    	}
-	        return sum;
-	    }
+int CameraController::getSum(int row) {
+    //the first array getting the camera input
+    int whiteness[320];
+    //the sum of all the white final numbers added
+    int sum = 0;
+    // a count of how many white spots have been counted( to check if we can still see the line)
+    int n_whites = 0;
+    take_picture();
+    for (int y = 0; y < row; y += 2) {
+        for (int i = 0; i < 320; i++) {
+            whiteness[i] = (get_pixel(i, y, 3) > WHITE_THRESHOLD);
+            n_whites += whiteness[i];
+            sum += whiteness[i] * (i - 160);
+        }
+    }
+    sum /= (row / 2);
+    // trying to make it so if it loses the line it reverses to try make it find it again
+    if (n_whites == 0) {
+        dir = 's';
+        return -1;
+    } else {
+        if (sum > 0) {
+            dir = 'l';
+        } else {
+            dir = 'r';
+        }
+        return sum;
+    }
 }
 
-int CameraController::update(int row){
-	//getWhiteArray();
-	int sum = getSum(row);
-	return sum;
-	//int diff = differential(sum);
-	//return motorMovement(sum, diff);
+int CameraController::update(int row) {
+    //getWhiteArray();
+    int sum = getSum(row);
+    return sum;
+    //int diff = differential(sum);
+    //return motorMovement(sum, diff);
 }
 
-char CameraController::getDir(){
-	return dir;
+char CameraController::getDir() {
+    return dir;
 }
 
 
