a67d4b1f62983c5c71ba2aac5ad37b9ef306cef5
mgoo <andrewmcghienz@gmail.com>
2016-05-24 22:47:18 +1200

    robot goes backwarsd when line is ends

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -10,6 +10,8 @@
 extern "C" int init(int d_lev);
 extern "C" int Sleep(int sec, int  usec);
 
+int forword = 1;
+
 int main(){
 	init(1);
 	NetworkController *network_controller = NetworkController::makeInstance("192.168.1.2",22);
@@ -21,10 +23,24 @@ int main(){
 	motor_controller->moveForward();
 
 	while (true) {
-		//printf("count: %d\n", count);
-		int sumC = camera_controller->update(CameraController::CENTER_ROW);
-		double pid_val = pid_controller->getPIDValue(sumC);
-		motor_controller->arc(pid_val);
+		try {
+			int sumC = camera_controller->update(CameraController::CENTER_ROW);
+			double pid_val = pid_controller->getPIDValue(sumC);
+			motor_controller->arc(pid_val,forword);
+			if (camera_controller->n_whites>3000){
+				forword=1;
+			}
+		} catch (int e){
+			if (e==1){ //if the robot losses the line
+				motor_controller->moveBackward();
+				printf("the camera lost the line!!");
+			} else if (e==2){
+				forword = 0;
+				motor_controller->moveBackward();
+			}
+		}
+		printf("n_whites: %d\n", camera_controller->n_whites);
+
 		//motor_controller->arc(camera_controller->getDir(), sumC);
 		printf("**************************************\n");//debuging print
 		//Sleep(0, 1e8);
