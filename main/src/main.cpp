8c13f2a15259913b187c215e42175943916d0568
Dylan Chong <dylan.chong@icloud.com>
2016-05-25 09:56:17 +1200

    Revert "Moved sleep millis up"
    This reverts commit f0ed8c22c1ef3055b26de36e1f6f44b34784cfdb.

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -10,12 +10,6 @@
 extern "C" int init(int d_lev);
 extern "C" int Sleep(int sec, int  usec);
 
-void sleepMillis(int millis) {
-	for (int a = 0; a < millis; a++) {
-		Sleep(0, 1000);
-	}
-}
-
 int main(){
 	init(1);
 	NetworkController *network_controller = NetworkController::makeInstance("192.168.1.2",22);
@@ -41,4 +35,10 @@ int main(){
 	printf("Program Ended\n");
 
 	return  0;
+}
+
+void sleepMillis(int millis) {
+	for (int a = 0; a < millis; a++) {
+		Sleep(0, 1000);
+	}
 }
