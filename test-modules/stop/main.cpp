ce05e893aeaa2f625115afff61ebe382535d2199
Dylan Chong <dylan.chong@icloud.com>
2016-05-25 12:49:37 +1200

    Edited stop code

--- test-modules/stop/main.cpp
+++ test-modules/stop/main.cpp
@@ -5,13 +5,22 @@ extern "C" int init(int x);
 extern "C" int Sleep(int sec, int usec);
 extern "C" int set_motor(int motor, int speed);
 
+void sleepMillis(int millis) {
+    for (int a = 0; a < millis; a++) {
+        Sleep(0, 1000);
+    }
+}
+
 int main() {
     init(0);
 
-    set_motor(1,0);
-    set_motor(2,0);
+    set_motor(1, 255);
+    set_motor(2, 255);
+    sleepMillis(400);
 
-    Sleep(1,0);
+    set_motor(1, 0);
+    set_motor(2, 0);
+    sleepMillis(400);
 
     return 0;
 }
